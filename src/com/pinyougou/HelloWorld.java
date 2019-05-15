3da9cce510d4a2442742e04816b51e038260b093
ling <2660302108@qq.com>
2019-05-15 21:31:20 +0800

    第一次修改

--- src/com/pinyougou/HelloWorld.java
+++ src/com/pinyougou/HelloWorld.java
@@ -3,5 +3,6 @@ package com.pinyougou;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("girl");
     }
 }
